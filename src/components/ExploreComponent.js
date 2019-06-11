084c5814861aba8a07ed63e60d69e975485e1735
jackvo123 <vthientong@gmail.com>
2019-06-11 21:27:25 +0700

    update

--- src/components/ExploreComponent.js
+++ src/components/ExploreComponent.js
@@ -5,36 +5,32 @@ class ExploreComponent extends React.Component {
         return (
             <div className="app-explore-component">
                 <div className="app-container">
-                    <div className="row row1">
+                    <div className="row app-explore-row">
                         <h2>Khám phá</h2>
                         <h3>Nơi công năng "chạm" xu hướng</h3>
                         <p>Lan toả sự dễ chịu, thanh thoát, các đại sảnh với thiết kế trần cao thoáng đãng, mang tông màu nền nã đan xen là chất liệu gỗ ấm áp kết hợp các chuỗi hạt ánh sáng lấp lánh, tất cả được khai triển tự do và xem trọng công năng biến hóa tùy ý.</p>
                     </div>
 
-                    <div className="row app-explore-row">
-                         
-                            <div className="app-explore-content">
-                            <div className="col-md-4 col1 app-explore-col">
-                                
-                            <div className="explore-item-image-background"></div>
-                            <img className="explore-item-image" src="images/Saltus.jpg"></img>
-                                
+                    <div className="row app-explore-image-row">
+                        <div className="app-explore-content">
+                            <div className="col-md-4 app-explore-col">
+                                <div className="explore-item-image-background"></div>
+                                <img className="explore-item-image" src="images/Saltus.jpg"></img>
                             </div>
-                            <div className="col-md-4 col2 app-explore-col">
-                               
+
+                            <div className="col-md-4 app-explore-col">
                                 <div className="explore-item-image-background"></div>
                                 <img className="explore-item-image" src="images/Sol.jpg"></img>
                             </div>
-                            <div className="col-md-4 col3 app-explore-col">
-                                
+
+                            <div className="col-md-4 app-explore-col">
                                 <div className="explore-item-image-background"></div>
                                 <img className="explore-item-image" src="images/CastorHome.jpg"></img>
                             </div>
-                            </div>
-                         
+                        </div>
                     </div>
 
-                    <div className="row row3">
+                    <div className="row">
                         <button>Khám phá</button>
                     </div>
                 </div>
--- src/pages/defaults/ContentComponent.js
+++ src/pages/defaults/ContentComponent.js
@@ -5,16 +5,15 @@ class ContentComponent extends React.Component {
         return (
             <div className="app-content-component">
                 <div className="app-container">
-                    <div className="row row1">
-                         
-                        <p>Chỉ cần chia sẻ với chúng tôi ý tưởng của bạn, <br/> GEM Center sẽ tạo điều kiện thuận lợi nhất để mong muốn của bạn có thể <br/> trở thành hiện thực.</p>
-                         
+                    <div className="row app-slogan-row">
+                        <p>Chỉ cần chia sẻ với chúng tôi ý tưởng của bạn, <br/> GEM Center sẽ tạo điều kiện thuận lợi nhất để mong muốn của bạn có thể <br/> trở thành hiện thực.</p> 
                     </div>
-                    <div className="row row2">
-                        <div className="col-md-6 col1">
-                                <img className="home1" src="images/sanh1.jpg"></img>
+                    
+                    <div className="row app-intro-row">
+                        <div className="col-md-6 app-intro-image-col">
+                                <img src="images/sanh1.jpg"></img>
                         </div>
-                        <div class="col-md-6 col2">
+                        <div class="col-md-6 app-intro-content-col">
                             <h2>Giới thiệu</h2>
                             <h3>Nơi tinh tế "chạm" cảm xúc</h3>
                             <p>GEM Center không ngừng nỗ lực mang đến cho khách hàng những trải nghiêm dịch vụ hoàn hảo. Từng chi tiết, dù là nhỏ nhất sẽ được chúng tôi chuẩn bị chu đáo, chăm chút tỉ mỉ. Các chuyên gia của chúng tôi sẽ cùng bạn xây dựng kế hoạch, tổ chức, trang trí và phục vụ yến tiệc để đảm bảo sự kiện của bạn sẽ được diễn ra thành công, ấn tượng và duy nhất như những gì bạn hình dung.</p>
